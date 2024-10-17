b05906ac2404e32760d69bcbbb0ba07dfb3908cc
chae45 <whwl45@naver.com>
2024-10-17 15:21:52 +0900

    Update DEV-STG-Peering.tf

--- DEV-STG-Peering.tf
+++ DEV-STG-Peering.tf
@@ -1,20 +1,20 @@
 # VPC Peering Connection: DEV to STG
 resource "aws_vpc_peering_connection" "DEV_STG_Peering" {
-  vpc_id      = aws_vpc.DEV_VPC.id
-  peer_vpc_id = aws_vpc.STG_VPC.id
+  vpc_id      = aws_vpc.DEV-VPC.id
+  peer_vpc_id = aws_vpc.STG-VPC.id
   auto_accept = true
 }
 
 # Route for DEV to STG
 resource "aws_route" "DEV_VPC_to_STG_VPC" {
-  route_table_id              = aws_route_table.DEV_RT_PUB.id
-  destination_cidr_block      = aws_vpc.STG_VPC.cidr_block
+  route_table_id              = aws_route_table.DEV-RT-PUB.id
+  destination_cidr_block      = aws_vpc.STG-VPC.cidr_block
   vpc_peering_connection_id    = aws_vpc_peering_connection.DEV_STG_Peering.id
 }
 
 # Route for STG to DEV
 resource "aws_route" "STG_VPC_to_DEV_VPC" {
-  route_table_id              = aws_route_table.STG_RT_PUB.id
-  destination_cidr_block      = aws_vpc.DEV_VPC.cidr_block
+  route_table_id              = aws_route_table.STG-RT-PUB.id
+  destination_cidr_block      = aws_vpc.DEV-VPC.cidr_block
   vpc_peering_connection_id    = aws_vpc_peering_connection.DEV_STG_Peering.id
 }
