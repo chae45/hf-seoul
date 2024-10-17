99bc80f1ed9d1af2287886021746daba4d0f50d6
chae45 <whwl45@naver.com>
2024-10-17 14:38:21 +0900

    Update STG_db.tf

--- STG_db.tf
+++ STG_db.tf
@@ -1,12 +1,12 @@
-# DB 서브넷 그룹
-resource "aws_db_subnet_group" "db_subnet" {
-  name       = "stg-hf-subnetgroup"
-  subnet_ids = [aws_subnet.STG-VPC-BASTION-PUB-2A.id, aws_subnet.STG-VPC-BASTION-PUB-2C.id, aws_subnet.STG-VPC-PRI-2A.id, aws_subnet.STG-VPC-PRI-2C.id]
-
-  tags = {
-    Name = "stg-hf-subnetgroup"
-  }
-}
+## DB 서브넷 그룹
+#resource "aws_db_subnet_group" "db_subnet" {
+#  name       = "stg-hf-subnetgroup"
+#  subnet_ids = [aws_subnet.STG-VPC-BASTION-PUB-2A.id, aws_subnet.STG-VPC-BASTION-PUB-2C.id, aws_subnet.STG-VPC-PRI-2A.id, aws_subnet.STG-VPC-PRI-2C.id]
+#
+#  tags = {
+#    Name = "stg-hf-subnetgroup"
+#  }
+#}
 
 # DB 구성
 resource "aws_db_instance" "db" {
